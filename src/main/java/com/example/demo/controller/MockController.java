769c5a498360ea667f57602f53d4cc7b761ae003
Akmal <akmal199818@gmail.com>
2022-11-28 21:33:37 +0500

    commit

--- src/main/java/com/example/demo/controller/MockController.java
+++ src/main/java/com/example/demo/controller/MockController.java
@@ -11,7 +11,7 @@ import java.util.*;
 
 @RestController
 @RequestMapping
-public class MockController { 
+public class MockController {
 
     @GetMapping(value = "get")
     public List<Map<String, String>> list(){
